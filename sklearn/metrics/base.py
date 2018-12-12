d9f1b63a16efe78f0cfa429bdb8098ddeaf91333
Thomas Fan <thomasjpfan@gmail.com>
2018-12-12 15:44:45 -0500

    WIP: Removes unrelated diffs

--- sklearn/metrics/base.py
+++ sklearn/metrics/base.py
@@ -34,7 +34,7 @@ def _average_binary_score(binary_metric, y_true, y_score, average,
         Target scores, can either be probability estimates of the positive
         class, confidence values, or binary decisions.
 
-    average : string, {None, 'micro', 'macro' (default), 'samples', 'weighted'},
+    average : string, [None, 'micro', 'macro' (default), 'samples', 'weighted']
         If ``None``, the scores for each class are returned. Otherwise,
         this determines the type of averaging performed on the data:
 
