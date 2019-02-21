2a9c566575ced3d3367e2117f0352607c2ff2a13
Thomas Fan <thomasjpfan@gmail.com>
2019-02-21 10:00:02 -0500

    TST Uses CODECOV_TOKEN

--- build_tools/azure/test_script.cmd
+++ build_tools/azure/test_script.cmd
@@ -4,7 +4,7 @@ mkdir %TMP_FOLDER%
 cd %TMP_FOLDER%
 
 if "%CHECK_WARNINGS%" == "true" (
-    pytest --junitxml=%JUNITXML% --showlocals --durations=20 -Werror::DeprecationWarning -Werror::FutureWarning --pyargs sklearn
+    pytest --junitxml=%JUNITXML% --showlocals --durations=20 -Werror::DeprecationWarning -Werror::FutureWarning --pyargs sklearn.preprocessing
 ) else (
-    pytest --junitxml=%JUNITXML% --showlocals --durations=20 --pyargs sklearn
+    pytest --junitxml=%JUNITXML% --showlocals --durations=20 --pyargs sklearn.preprocessing
 )
