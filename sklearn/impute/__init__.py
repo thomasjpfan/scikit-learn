157c307c380eb2fba04edbefe4c69737c67da9f9
Thomas Fan <thomasjpfan@gmail.com>
2019-05-28 14:01:42 -0400

    CLN Updates __all__

--- sklearn/impute/__init__.py
+++ sklearn/impute/__init__.py
@@ -1,8 +1,9 @@
 """Transformers for missing value imputation"""
 
-from ._base import MissingIndicator, SimpleImputer
+from ._base import MissingIndicator, SimpleImputer, KNNImputer
 
 __all__ = [
     'MissingIndicator',
     'SimpleImputer',
+    'KNNImputer'
 ]
