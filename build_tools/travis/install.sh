55385f66470cf1539d2e863a58eddc0557254189
Thomas J Fan <thomasjpfan@gmail.com>
2019-11-19 15:24:28 -0500

    TST Get python version [arm-dev]

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -16,6 +16,8 @@ set -e
 # Fail fast
 # build_tools/travis/travis_fastfail.sh
 
+python3 --version
+
 echo "List files from cached directories"
 echo "pip:"
 ls $HOME/.cache/pip
@@ -27,15 +29,19 @@ export CXX=/usr/lib/ccache/g++
 # ~60M is used by .ccache when compiling from scratch at the time of writing
 ccache --max-size 100M --show-stats
 
-pip3 install --upgrade pip setuptools
-echo "Installing numpy and scipy master wheels"
-dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
-pip3 install --pre --upgrade --timeout=60 -f $dev_url numpy scipy pandas cython
-echo "Installing joblib master"
-pip3 install https://github.com/joblib/joblib/archive/master.zip
-echo "Installing pillow master"
-pip3 install https://github.com/python-pillow/Pillow/archive/master.zip
-pip3 install pytest==4.6.4 pytest-cov
+if [ $TRAVIS_CPU_ARCH != "amd64" ]; then
+    pip3 install --upgrade pip setuptools
+    echo "Installing numpy and scipy master wheels"
+    dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
+    pip3 install --pre --upgrade --timeout=60 -f $dev_url numpy scipy pandas cython
+    echo "Installing joblib master"
+    pip3 install https://github.com/joblib/joblib/archive/master.zip
+    echo "Installing pillow master"
+    pip3 install https://github.com/python-pillow/Pillow/archive/master.zip
+    pip3 install pytest==4.6.4 pytest-cov
+fi
+
+
 
 # Build scikit-learn in the install.sh script to collapse the verbose
 # build output in the travis output when it succeeds.
