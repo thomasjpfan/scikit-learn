ef0ab231c365531cd01db963a38610d6417ce8b0
Thomas J. Fan <thomasjpfan@gmail.com>
2020-05-23 22:24:06 -0400

    WIP Try with non master joblib

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -109,7 +109,8 @@ elif [[ "$DISTRIB" == "conda-pip-scipy-dev" ]]; then
     dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
     pip install --pre --upgrade --timeout=60 -f $dev_url numpy scipy pandas cython
     echo "Installing joblib master"
-    pip install https://github.com/joblib/joblib/archive/master.zip
+    # pip install https://github.com/joblib/joblib/archive/master.zip
+    pip install joblib
     echo "Installing pillow master"
     pip install https://github.com/python-pillow/Pillow/archive/master.zip
 fi
