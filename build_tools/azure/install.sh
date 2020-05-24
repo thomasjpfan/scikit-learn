e2f3a86dc995eccaf636147a96c2491a951c1842
Thomas J. Fan <thomasjpfan@gmail.com>
2020-05-24 16:30:44 -0400

    PIN

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -107,11 +107,10 @@ elif [[ "$DISTRIB" == "conda-pip-scipy-dev" ]]; then
     python -m pip install pytest==$PYTEST_VERSION pytest-cov pytest-xdist
     echo "Installing numpy and scipy master wheels"
     dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
-    pip install --pre --upgrade --timeout=60 -f $dev_url numpy scipy
-    pip install cython
+    pip install --pre --upgrade --timeout=60 -f $dev_url scipy cython
+    pip install numpy
     echo "Installing joblib master"
     pip install https://github.com/joblib/joblib/archive/master.zip
-    pip install joblib
     echo "Installing pillow master"
     pip install https://github.com/python-pillow/Pillow/archive/master.zip
 fi
