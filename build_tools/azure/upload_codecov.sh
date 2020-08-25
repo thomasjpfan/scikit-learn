45bbe16423491ef63192523b7d6cf3c6b4139854
Thomas J. Fan <thomasjpfan@gmail.com>
2020-08-25 17:45:06 -0400

    CI Upload to codecov

--- build_tools/azure/upload_codecov.sh
+++ build_tools/azure/upload_codecov.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-# called when COVERAGE=="true" and DISTRIB=="conda"
-export PATH=$HOME/miniconda3/bin:$PATH
-source activate $VIRTUALENV
+if [[ "$DISTRIB" == "conda" ]]; then
+    export PATH=$HOME/miniconda3/bin:$PATH
+    source activate $VIRTUALENV
+else
+    source $VIRTUALENV/bin/activate
+fi
 
 # Need to run codecov from a git checkout, so we copy .coverage
 # from TEST_DIR where pytest has been run
