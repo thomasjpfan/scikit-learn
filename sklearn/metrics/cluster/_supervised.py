9c3949079c8afa4bab85b52304ae27beeff60bb2
Thomas J. Fan <thomasjpfan@gmail.com>
2021-01-14 17:52:41 -0500

    WIP More debugging

--- sklearn/metrics/cluster/_supervised.py
+++ sklearn/metrics/cluster/_supervised.py
@@ -790,10 +790,8 @@ def mutual_info_score(labels_true, labels_pred, *, contingency=None):
     log_contingency_nm = np.log(nz_val)
     contingency_nm = nz_val / contingency_sum
     # Don't need to calculate the full outer product, just for non-zeroes
-    outer = (pi.take(nzx).astype(np.int64, copy=False)
-             * pj.take(nzy).astype(np.int64, copy=False))
+    outer = pi.take(nzx) * pj.take(nzy)
     log_outer = -np.log(outer) + log(pi.sum()) + log(pj.sum())
-    print(contingency_nm, log_contingency_nm, log(contingency_sum), log_outer)
     mi = (contingency_nm * (log_contingency_nm - log(contingency_sum)) +
           contingency_nm * log_outer)
     return np.clip(mi.sum(), 0.0, None)
