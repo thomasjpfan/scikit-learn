aeb668760812d7d66158b34088eae001004420d6
Thomas J. Fan <thomasjpfan@gmail.com>
2021-01-15 11:02:23 -0500

    WIP Use around instead

--- sklearn/metrics/cluster/_supervised.py
+++ sklearn/metrics/cluster/_supervised.py
@@ -795,7 +795,7 @@ def mutual_info_score(labels_true, labels_pred, *, contingency=None):
     log_outer = -np.log(outer) + log(pi.sum()) + log(pj.sum())
     mi = (contingency_nm * (log_contingency_nm - log(contingency_sum)) +
           contingency_nm * log_outer)
-    np.around(mi, decimals=np.finfo('float64').precision, out=mi)
+    np.around(mi, decimals=np.finfo(mi.dtype).precision, out=mi)
     return np.clip(mi.sum(), 0.0, None)
 
 
