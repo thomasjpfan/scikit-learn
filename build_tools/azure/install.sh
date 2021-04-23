9fb19ffcbd65ba08cc1f95aed7433918e30cd8cc
Thomas J. Fan <thomasjpfan@gmail.com>
2021-04-23 12:37:43 -0400

    CI Do not install openmp for osx

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -45,12 +45,14 @@ if [[ "$DISTRIB" == "conda" ]]; then
     TO_INSTALL="$TO_INSTALL $(get_dep matplotlib $MATPLOTLIB_VERSION)"
 
     if [[ "$UNAMESTR" == "Darwin" ]]; then
+        # on macOS, install an OpenMP-enabled clang/llvm from conda-forge.
+        # TODO: Remove !=1.1.0 when the following is fixed:
+        # sklearn/svm/_libsvm.cpython-38-darwin.so,
+        # 2): Symbol not found: _svm_check_parameter error
+        TO_INSTALL="$TO_INSTALL compilers>=1.0.4,!=1.1.0"
+
         if [[ "$SKLEARN_TEST_NO_OPENMP" != "true" ]]; then
-            # on macOS, install an OpenMP-enabled clang/llvm from conda-forge.
-            # TODO: Remove !=1.1.0 when the following is fixed:
-            # sklearn/svm/_libsvm.cpython-38-darwin.so,
-            # 2): Symbol not found: _svm_check_parameter error
-            TO_INSTALL="$TO_INSTALL compilers>=1.0.4,!=1.1.0 llvm-openmp"
+            TO_INSTALL="$TO_INSTALL llvm-openmp"
         fi
     fi
 	make_conda $TO_INSTALL
