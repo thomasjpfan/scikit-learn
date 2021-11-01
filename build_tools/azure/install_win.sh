50c34ba9d236561270a865bccb70b9e90c2f3a52
Thomas J. Fan <thomasjpfan@gmail.com>
2021-11-01 10:38:23 -0400

    ENH Faaster

--- build_tools/azure/install_win.sh
+++ build_tools/azure/install_win.sh
@@ -3,34 +3,34 @@
 set -e
 set -x
 
-if [[ "$PYTHON_ARCH" == "64" ]]; then
-    # conda create -n $VIRTUALENV -q -y python=$PYTHON_VERSION numpy scipy cython
+# if [[ "$PYTHON_ARCH" == "64" ]]; then
+#     # conda create -n $VIRTUALENV -q -y python=$PYTHON_VERSION numpy scipy cython
 
-    # source activate $VIRTUALENV
+#     # source activate $VIRTUALENV
 
-    pip install threadpoolctl scipy numpy cython
+#     pip install threadpoolctl scipy numpy cython
 
-    if [[ "$PYTEST_VERSION" == "*" ]]; then
-        pip install pytest
-    else
-        pip install pytest==$PYTEST_VERSION
-    fi
-else
-    pip install numpy scipy cython pytest wheel pillow joblib threadpoolctl
-fi
+#     if [[ "$PYTEST_VERSION" == "*" ]]; then
+#         pip install pytest
+#     else
+#         pip install pytest==$PYTEST_VERSION
+#     fi
+# else
+#     pip install numpy scipy cython pytest wheel pillow joblib threadpoolctl
+# fi
 
-if [[ "$PYTEST_XDIST_VERSION" != "none" ]]; then
-    pip install pytest-xdist
-fi
+# if [[ "$PYTEST_XDIST_VERSION" != "none" ]]; then
+#     pip install pytest-xdist
+# fi
 
-if [[ "$COVERAGE" == "true" ]]; then
-    pip install coverage codecov pytest-cov
-fi
+# if [[ "$COVERAGE" == "true" ]]; then
+#     pip install coverage codecov pytest-cov
+# fi
 
-python --version
-pip --version
+# python --version
+# pip --version
 
-pip install scikit-learn
+# pip install scikit-learn
 
 # Build scikit-learn
 # python setup.py bdist_wheel
@@ -38,7 +38,7 @@ pip install scikit-learn
 # Install the generated wheel package to test it
 # pip install --pre --no-index --find-links dist scikit-learn
 
-TEST_CMD="python -m pytest --showlocals --durations=20 --junitxml=$JUNITXML"
+TEST_CMD="python -m pytest --showlocals --durations=20"
 # Python 3.10 deprecates disutils and is imported by numpy interally during import time
 PYTHON_3_10_WARNING="-W ignore:'The distutils':DeprecationWarning"
 TEST_CMD="$TEST_CMD $PYTHON_3_10_WARNING"
