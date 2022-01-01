5252cbf1113f7cbea45a1b1903ba3cc597fcd10a
Thomas J. Fan <thomasjpfan@gmail.com>
2022-01-01 09:15:28 -0500

    ENH Actually build

--- build_tools/github/build_wheels.sh
+++ build_tools/github/build_wheels.sh
@@ -25,17 +25,13 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
     fi
 
     conda install $OPENMP_URL
-    echo $CONDA_PREFIX
-    ls $CONDA_PREFIX/include
-    ls $CONDA_PREFIX/lib
-    exit 1
 
     export CC=/usr/bin/clang
     export CXX=/usr/bin/clang++
     export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
-    export CFLAGS="$CFLAGS -I/usr/local/miniconda/include"
-    export CXXFLAGS="$CXXFLAGS -I/usr/local/miniconda/include"
-    export LDFLAGS="$LDFLAGS -Wl,-rpath,/usr/local/miniconda/lib -L/usr/local/miniconda/lib -lomp"
+    export CFLAGS="$CFLAGS -I$CONDA_PREFIX/include"
+    export CXXFLAGS="$CXXFLAGS -I$CONDA_PREFIX/include"
+    export LDFLAGS="$LDFLAGS -Wl,-rpath,$CONDA_PREFIX/include/lib -L$CONDA_PREFIX/include/lib -lomp"
 fi
 
 # The version of the built dependencies are specified
