a5d83c027bdb7650d23bf1ebde526f06e9d5246d
Thomas J. Fan <thomasjpfan@gmail.com>
2022-09-12 11:38:13 -0400

    FIX Patch everything

--- sklearn/_build_utils/patch_new_compiler.py
+++ sklearn/_build_utils/patch_new_compiler.py
@@ -1,6 +1,9 @@
 """Patches new_compiler to supported compilers."""
+from contextlib import suppress
+from importlib import import_module
+
 from setuptools.command.build_ext import new_compiler as orig_new_compiler
-import setuptools.command.build_ext
+
 from .intelccompiler import IntelEM64TCCompiler
 
 
@@ -13,4 +16,13 @@ def patched_new_compiler(plat=None, compiler=None, verbose=0, dry_run=0, force=0
 
 
 def patch_new_compiler():
-    setuptools.command.build_ext.new_compiler = patched_new_compiler
+    patch_modules = [
+        "setuptools.command.build_ext",
+        "distutils.ccompiler",
+        "setuptools._distutils.ccompiler",
+    ]
+
+    for patch_module in patch_modules:
+        with suppress(ImportError):
+            mod = import_module(patch_module)
+            mod.new_compiler = patch_new_compiler
