7e4e808afe57ad8e7af953f66a96c42cd22e9501
Thomas J. Fan <thomasjpfan@gmail.com>
2022-09-29 14:57:56 -0400

    BLD Build with only setuptools

--- sklearn/_build_utils/pre_build_helpers.py
+++ sklearn/_build_utils/pre_build_helpers.py
@@ -7,16 +7,54 @@ import tempfile
 import textwrap
 import setuptools  # noqa
 import subprocess
-import warnings
 
-from distutils.dist import Distribution
-from distutils.sysconfig import customize_compiler
+from setuptools import Distribution, Command
+from setuptools.command.build_ext import customize_compiler, new_compiler
 
-# NumPy 1.23 deprecates numpy.distutils
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    from numpy.distutils.ccompiler import new_compiler
-    from numpy.distutils.command.config_compiler import config_cc
+
+class config_cc(Command):
+    """Distutils command to hold user specified options
+    to C/C++ compilers.
+    """
+
+    description = "specify C/C++ compiler information"
+
+    user_options = [
+        ("compiler=", None, "specify C/C++ compiler type"),
+    ]
+
+    def initialize_options(self):
+        self.compiler = None
+
+    def finalize_options(self):
+
+        build_clib = self.get_finalized_command("build_clib")
+        build_ext = self.get_finalized_command("build_ext")
+        config = self.get_finalized_command("config")
+        build = self.get_finalized_command("build")
+        cmd_list = [self, config, build_clib, build_ext, build]
+        for a in ["compiler"]:
+            l = []
+            for c in cmd_list:
+                v = getattr(c, a)
+                if v is not None:
+                    if not isinstance(v, str):
+                        v = v.compiler_type
+                    if v not in l:
+                        l.append(v)
+            if not l:
+                v1 = None
+            else:
+                v1 = l[0]
+            if len(l) > 1:
+                for c in cmd_list:
+                    if getattr(c, a) is None:
+                        setattr(c, a, v1)
+        return
+
+    def run(self):
+        # Do nothing.
+        return
 
 
 def _get_compiler():
@@ -37,8 +75,12 @@ def _get_compiler():
     dist.parse_command_line()
 
     cmd_opts = dist.command_options.get("build_ext")
+    cmd_opts2 = dist.command_options.get("build")
+
     if cmd_opts is not None and "compiler" in cmd_opts:
         compiler = cmd_opts["compiler"][1]
+    elif cmd_opts2 is not None and "compiler" in cmd_opts2:
+        compiler = cmd_opts2["compiler"][1]
     else:
         compiler = None
 
