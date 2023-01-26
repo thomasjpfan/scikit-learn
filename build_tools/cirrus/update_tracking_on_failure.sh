04294831a2840aa715ebe38a9b0ee027d063c513
Thomas J. Fan <thomasjpfan@gmail.com>
2023-01-26 12:06:53 -0500

    CI Trigger on failure

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -6,9 +6,16 @@ source .venv/bin/activate
 python -m pip install defusedxml PyGithub
 
 LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
-ISSUE_REPO="$CIRRUS_REPO_FULL_NAME"
 CI_NAME="$1"
 
 echo $LINK_TO_RUN
 echo $CIRRUS_REPO_FULL_NAME
+echo $CIRRUS_TASK_NAME
 echo $CI_NAME
+
+# python -m maint_tools/update_tracking_issue.py \
+#    $BOT_GITHUB_TOKEN \
+#    $CI_NAME \
+#    $CIRRUS_REPO_FULL_NAME \
+#    $LINK_TO_RUN
+#    --tests-passed false
