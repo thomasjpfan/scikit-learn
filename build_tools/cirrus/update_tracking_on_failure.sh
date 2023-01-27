314ba964ec094c095dfa1afa1549eba8216f60bf
Thomas J. Fan <thomasjpfan@gmail.com>
2023-01-27 18:03:17 -0500

    CI Try with unknown env

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -1,21 +1,21 @@
 # Update tracking issue if Cirrus fails nightly job
 
+# Only update for nightly jobs
 echo $CIRRUS_CRON
+
 python -m venv .venv
 source .venv/bin/activate
 python -m pip install defusedxml PyGithub
 
 LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
-CI_NAME="$1"
 
 echo $LINK_TO_RUN
 echo $CIRRUS_REPO_FULL_NAME
-echo $CIRRUS_TASK_NAME
 echo $CI_NAME
 
 # python -m maint_tools/update_tracking_issue.py \
 #    $BOT_GITHUB_TOKEN \
-#    $CI_NAME \
+#    $CIRRUS_TASK_NAME \
 #    $CIRRUS_REPO_FULL_NAME \
 #    $LINK_TO_RUN
 #    --tests-passed false
