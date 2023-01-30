1cf9d3678fcab8e32cdc1e0fa325cb4b0bf89b35
Thomas J. Fan <thomasjpfan@gmail.com>
2023-01-30 14:27:32 -0500

    CI Try update

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -2,16 +2,15 @@
 
 # Only update for nightly jobs
 echo $CIRRUS_CRON
+if [[ -z $BOT_GITHUB_TOKEN ]]; then
+   exit 0
+fi
 
 python -m venv .venv
 source .venv/bin/activate
 python -m pip install defusedxml PyGithub
 
 LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
-
-echo $LINK_TO_RUN
-echo $CIRRUS_REPO_FULL_NAME
-echo $CI_NAME
 
 python maint_tools/update_tracking_issue.py \
    $BOT_GITHUB_TOKEN \
