87fd407e8ccba19b47ee35757701d4d175ecc5ae
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-14 13:59:39 -0500

    CI Update status

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -1,20 +1,22 @@
 # Update tracking issue if Cirrus fails nightly job
 
-# Only update for nightly jobs
-echo $CIRRUS_CRON
-if [[ -z $BOT_GITHUB_TOKEN ]]; then
-   exit 0
-fi
+curl https://api.cirrus-ci.com/v1/artifact/build/$CIRRUS_BUILD_ID/passed.zip --output passed.zip
+unzip passed.zip
+PASSED=$(cat passed.txt)
 
-python -m venv .venv
-source .venv/bin/activate
 python -m pip install defusedxml PyGithub
 
 LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
 
 python maint_tools/update_tracking_issue.py \
    $BOT_GITHUB_TOKEN \
-   $CIRRUS_TASK_NAME \
+   "ARM Wheels" \
    $CIRRUS_REPO_FULL_NAME \
    $LINK_TO_RUN \
    --tests-passed false
+
+if [[ "$PASSED" == "true" ]]; then
+   exit 0
+else
+   exit 1
+fi
