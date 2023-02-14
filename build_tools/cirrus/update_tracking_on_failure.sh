7211c727bef75c5de28732ac2f9ac87d50c0053c
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-14 14:19:29 -0500

    CI Update status

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -2,24 +2,31 @@
 
 curl https://api.cirrus-ci.com/v1/artifact/build/$CIRRUS_BUILD_ID/status.zip --output status.zip
 unzip status.zip
+PASSED_LIST=$(find status -type f -exec cat {} +)
 
-ls status
+ALL_PASSED="true"
+for PASSED in $PASSED_LIST; do
+   if [[ "$passed" == "false" ]]; then
+      ALL_PASSED="false"
+      break
+   fi
+done
 
-# python -m venv .venv
-# source .venv/bin/activate
-# python -m pip install defusedxml PyGithub
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install defusedxml PyGithub
 
-# LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
+LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
 
-# python maint_tools/update_tracking_issue.py \
-#    $BOT_GITHUB_TOKEN \
-#    "ARM Wheels" \
-#    $CIRRUS_REPO_FULL_NAME \
-#    $LINK_TO_RUN \
-#    --tests-passed false
+python maint_tools/update_tracking_issue.py \
+   $BOT_GITHUB_TOKEN \
+   "ARM Wheels" \
+   $CIRRUS_REPO_FULL_NAME \
+   $LINK_TO_RUN \
+   --tests-passed $ALL_PASSED
 
-# if [[ "$PASSED" == "true" ]]; then
-#    exit 0
-# else
-#    exit 1
-# fi
+if [[ "$ALL_PASSED" == "true" ]]; then
+   exit 0
+else
+   exit 1
+fi
