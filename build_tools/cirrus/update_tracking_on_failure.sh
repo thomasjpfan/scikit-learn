ceff57c5659a9e6fbd2d375fb576cdd349377a37
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-14 14:52:30 -0500

    CI All pass

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -14,23 +14,21 @@ for PASSED in $PASSED_LIST; do
    fi
 done
 
-echo $ALL_PASSED
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install defusedxml PyGithub
 
-# python -m venv .venv
-# source .venv/bin/activate
-# python -m pip install defusedxml PyGithub
+LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
 
-# LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
+python maint_tools/update_tracking_issue.py \
+   $BOT_GITHUB_TOKEN \
+   "ARM Wheels" \
+   $CIRRUS_REPO_FULL_NAME \
+   $LINK_TO_RUN \
+   --tests-passed $ALL_PASSED
 
-# python maint_tools/update_tracking_issue.py \
-#    $BOT_GITHUB_TOKEN \
-#    "ARM Wheels" \
-#    $CIRRUS_REPO_FULL_NAME \
-#    $LINK_TO_RUN \
-#    --tests-passed $ALL_PASSED
-
-# if [[ "$ALL_PASSED" == "true" ]]; then
-#    exit 0
-# else
-#    exit 1
-# fi
+if [[ "$ALL_PASSED" == "true" ]]; then
+   exit 0
+else
+   exit 1
+fi
