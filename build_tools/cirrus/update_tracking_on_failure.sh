3f0963b2be989a5683292da438ba2caded5ebc9a
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-14 21:15:16 -0500

    CI Testing checks

--- build_tools/cirrus/update_tracking_on_failure.sh
+++ build_tools/cirrus/update_tracking_on_failure.sh
@@ -1,22 +1,20 @@
 # Update tracking issue if Cirrus fails nightly job
 
-ALL_PASSED=$(bash build_tools/cirrus/jobs_all_passed.sh)
+# if [[ "$CIRRUS_CRON" != "nightly" ]]; then
+#     exit 0
+# fi
 
 python -m venv .venv
 source .venv/bin/activate
 python -m pip install defusedxml PyGithub
 
-LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
+echo $CIRRUS_TASK_NAME
 
-python maint_tools/update_tracking_issue.py \
-    $BOT_GITHUB_TOKEN \
-    "ARM Wheels" \
-    $CIRRUS_REPO_FULL_NAME \
-    $LINK_TO_RUN \
-    --tests-passed $ALL_PASSED
+# LINK_TO_RUN="https://cirrus-ci.com/build/$CIRRUS_BUILD_ID"
 
-if [[ "$ALL_PASSED" == "true" ]]; then
-    exit 0
-else
-    exit 1
-fi
+# python maint_tools/update_tracking_issue.py \
+#     $BOT_GITHUB_TOKEN \
+#     $CIRRUS_TASK_NAME \
+#     $CIRRUS_REPO_FULL_NAME \
+#     $LINK_TO_RUN \
+#     --tests-passed "false"
