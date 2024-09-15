a70fec7bb1452471461304dc802ba0a1c87f4763
Thomas J. Fan <thomasjpfan@gmail.com>
2024-09-15 08:33:08 -0700

    Actually check license

--- build_tools/github/build_minimal_windows_image.sh
+++ build_tools/github/build_minimal_windows_image.sh
@@ -5,23 +5,21 @@ set -x
 
 PYTHON_VERSION=$1
 
-python build_tools/wheels/check_license.py
+TEMP_FOLDER="$HOME/AppData/Local/Temp"
+WHEEL_PATH=$(ls -d $TEMP_FOLDER/**/*/repaired_wheel/*)
+WHEEL_NAME=$(basename $WHEEL_PATH)
 
-# TEMP_FOLDER="$HOME/AppData/Local/Temp"
-# WHEEL_PATH=$(ls -d $TEMP_FOLDER/**/*/repaired_wheel/*)
-# WHEEL_NAME=$(basename $WHEEL_PATH)
+cp $WHEEL_PATH $WHEEL_NAME
 
-# cp $WHEEL_PATH $WHEEL_NAME
+# Dot the Python version for identyfing the base Docker image
+PYTHON_VERSION=$(echo ${PYTHON_VERSION:0:1}.${PYTHON_VERSION:1:2})
 
-# # Dot the Python version for identyfing the base Docker image
-# PYTHON_VERSION=$(echo ${PYTHON_VERSION:0:1}.${PYTHON_VERSION:1:2})
-
-# if [[ "$CIBW_PRERELEASE_PYTHONS" == "True" ]]; then
-#     PYTHON_VERSION="$PYTHON_VERSION-rc"
-# fi
-# # Build a minimal Windows Docker image for testing the wheels
-# docker build --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-#              --build-arg WHEEL_NAME=$WHEEL_NAME \
-#              --build-arg CIBW_TEST_REQUIRES="$CIBW_TEST_REQUIRES" \
-#              -f build_tools/github/Windows \
-#              -t scikit-learn/minimal-windows .
+if [[ "$CIBW_PRERELEASE_PYTHONS" == "True" ]]; then
+    PYTHON_VERSION="$PYTHON_VERSION-rc"
+fi
+# Build a minimal Windows Docker image for testing the wheels
+docker build --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+             --build-arg WHEEL_NAME=$WHEEL_NAME \
+             --build-arg CIBW_TEST_REQUIRES="$CIBW_TEST_REQUIRES" \
+             -f build_tools/github/Windows \
+             -t scikit-learn/minimal-windows .
--- build_tools/github/test_windows_wheels.sh
+++ build_tools/github/test_windows_wheels.sh
@@ -5,11 +5,13 @@ set -x
 
 PYTHON_VERSION=$1
 
-docker container run \
-    --rm scikit-learn/minimal-windows \
-    powershell -Command "python -c 'import sklearn; sklearn.show_versions()'"
+python build_tools/wheels/check_license.py
 
-docker container run \
-    -e SKLEARN_SKIP_NETWORK_TESTS=1 \
-    --rm scikit-learn/minimal-windows \
-    powershell -Command "pytest --pyargs sklearn"
+# docker container run \
+#     --rm scikit-learn/minimal-windows \
+#     powershell -Command "python -c 'import sklearn; sklearn.show_versions()'"
+
+# docker container run \
+#     -e SKLEARN_SKIP_NETWORK_TESTS=1 \
+#     --rm scikit-learn/minimal-windows \
+#     powershell -Command "pytest --pyargs sklearn"
